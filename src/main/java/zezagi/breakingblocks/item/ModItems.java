9a64426d0ec1649fbe1281e8eb02fade1c70ffa3
Tymek <tymgar000@pbs.edu.pl>
2026-02-15 04:01:38 +0100

    coca plants working, coca leaf, maceration barrel in mgmt

--- src/main/java/zezagi/breakingblocks/item/ModItems.java
+++ src/main/java/zezagi/breakingblocks/item/ModItems.java
@@ -1,10 +1,7 @@
 package zezagi.breakingblocks.item;
 
-import net.fabricmc.fabric.api.block.v1.FabricBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -15,43 +12,58 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import zezagi.breakingblocks.BreakingBlocks;
 import zezagi.breakingblocks.item.customItem.CocaCropBlock;
+import zezagi.breakingblocks.item.customItem.MacerationBarrelBlock;
+
 import java.util.function.Function;
 
 public class ModItems {
 
     public static final Block COCA_CROP = registerBlockWithoutItem("coca_crop", key ->
-            new CocaCropBlock(AbstractBlock.Settings.copy(Blocks.WHEAT)
-                    .registryKey(key)
-                    .nonOpaque()
-                    .noCollision()
-                    .breakInstantly()
-            )
+            new CocaCropBlock(
+                    Block.Settings.create()
+                            .registryKey(key)
+                            .noCollision()
+                            .ticksRandomly()
+                            .breakInstantly()
+                            .sounds(net.minecraft.sound.BlockSoundGroup.CROP))
     );
+
     public static final Item COCA_SEEDS = registerItem("coca_seeds", settings -> new BlockItem(ModItems.COCA_CROP, settings));
 
-    private static Item registerItem(String name, java.util.function.Function<Item.Settings, Item> itemFactory) {
+    public static final Item COCA_LEAF = registerItem("coca_leaf", Item::new);
+
+    public static final Block MACERATION_BARREL = registerBlockWithoutItem("maceration_barrel", key ->
+            new MacerationBarrelBlock(Block.Settings.create()
+                    .registryKey(key)
+                    .nonOpaque()
+                    .strength(2.0f)
+                    .sounds(net.minecraft.sound.BlockSoundGroup.WOOD))
+    );
+
+    public static final Item MACERATION_BARREL_ITEM = registerItem("maceration_barrel", settings -> new BlockItem(MACERATION_BARREL, settings));
+
+
+    private static Item registerItem(String name, Function<Item.Settings, Item> itemFactory) {
         Identifier id = Identifier.of(BreakingBlocks.MOD_ID, name);
         RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
-
         Item.Settings settings = new Item.Settings().registryKey(key);
-
         Item item = itemFactory.apply(settings);
-
         return Registry.register(Registries.ITEM, id, item);
     }
 
     private static <T extends Block> T registerBlockWithoutItem(String name, Function<RegistryKey<Block>, T> blockFactory) {
         Identifier id = Identifier.of(BreakingBlocks.MOD_ID, name);
         RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, id);
-
         T block = blockFactory.apply(key);
         return Registry.register(Registries.BLOCK, id, block);
     }
 
     public static void registerModItems() {
-        BreakingBlocks.LOGGER.info("Registering Mod Items for"+BreakingBlocks.MOD_ID);
+        BreakingBlocks.LOGGER.info("Registering Mod Items for " + BreakingBlocks.MOD_ID);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(COCA_SEEDS);
+            entries.add(COCA_LEAF);
+            entries.add(MACERATION_BARREL_ITEM);
         });
     }
 }
--- src/main/java/zezagi/breakingblocks/item/customItem/CocaCropBlock.java
+++ src/main/java/zezagi/breakingblocks/item/customItem/CocaCropBlock.java
@@ -1,16 +1,151 @@
 package zezagi.breakingblocks.item.customItem;
 
-import net.minecraft.block.CropBlock;
-import net.minecraft.item.ItemConvertible;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.PlantBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import zezagi.breakingblocks.item.ModItems;
 
-public class CocaCropBlock extends CropBlock {
+public class CocaCropBlock extends PlantBlock implements Fertilizable {
+
+    public static final MapCodec<CocaCropBlock> CODEC = createCodec(CocaCropBlock::new);
+
+    public static final int MAX_AGE = 10;
+    public static final IntProperty AGE = IntProperty.of("age", 0, MAX_AGE);
+
+    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 10.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 32.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 32.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 32.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 32.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 32.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 32.0, 16.0)
+    };
+
     public CocaCropBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(AGE, 0));
     }
 
     @Override
-    protected ItemConvertible getSeedsItem() {
-        return ModItems.COCA_SEEDS;
+    public MapCodec<CocaCropBlock> getCodec() {
+        return CODEC;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[state.get(AGE)];
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+
+    @Override
+    protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+        return floor.isOf(Blocks.FARMLAND);
+    }
+
+    @Override
+    protected ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        int age = state.get(AGE);
+
+        if (stack.isOf(Items.SHEARS) && age == 7) {
+            if (!world.isClient()) {
+                world.setBlockState(pos, state.with(AGE, 8), Block.NOTIFY_LISTENERS);
+                HandleDrop(world, pos);
+                world.playSound(null, pos, SoundEvents.ENTITY_SHEEP_SHEAR, SoundCategory.BLOCKS, 1.0f, 1.0f);
+            }
+            return ActionResult.SUCCESS;
+        }
+
+        return super.onUseWithItem(stack, state, world, pos, player, hand, hit);
+    }
+
+    private void HandleDrop(World world, BlockPos pos) {
+        dropStack(world, pos, new ItemStack(ModItems.COCA_SEEDS, world.getRandom().nextInt(2)));
+        dropStack(world, pos, new ItemStack(ModItems.COCA_LEAF, 5 + world.getRandom().nextInt(5)));
+    }
+
+    @Override
+    public boolean hasRandomTicks(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (world.getBaseLightLevel(pos, 0) >= 9) {
+            int age = state.get(AGE);
+            if (random.nextInt(5) == 0) {
+                if (age == 10) {
+                    world.setBlockState(pos, state.with(AGE, 5), Block.NOTIFY_LISTENERS);
+                } else if (age < 10 && age != 7) {
+                    world.setBlockState(pos, state.with(AGE, age + 1), Block.NOTIFY_LISTENERS);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
+        return state.get(AGE) != 7;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        int age = state.get(AGE);
+        int growth = 1 + random.nextInt(2);
+
+        if(world.getBaseLightLevel(pos, 0) < 7)
+        {
+            world.breakBlock(pos, true);
+            return;
+        }
+
+        if (age >= 8) {
+            int newAge = age + growth;
+            if (newAge > 10) {
+                world.setBlockState(pos, state.with(AGE, 5), Block.NOTIFY_LISTENERS);
+            } else {
+                world.setBlockState(pos, state.with(AGE, newAge), Block.NOTIFY_LISTENERS);
+            }
+        } else if (age < 7) {
+            int newAge = age + growth;
+            if (newAge > 7) {
+                newAge = 7;
+            }
+            world.setBlockState(pos, state.with(AGE, newAge), Block.NOTIFY_LISTENERS);
+        }
     }
 }
